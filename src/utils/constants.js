efc0d41af00fe44686481bfc1db0c5e660de6558
Freddy PC <freddythepc@gmail.com>
2023-06-14 23:31:20 -0400

    Add array with pok-type to color relation

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+const colors = {
+  grass: "green",
+  fire: "orangered",
+  water: "deepskyblue",
+
+  normal: "#A8A878",
+  fighting: "#C03028",
+  ghost: "darkslateblue",
+
+  electric: "#F8D030",
+  ice: "lightskyblue",
+  rock: "#B8A015",
+
+  poison: "#A040A0",
+  ground: "#E0C065",
+  flying: "#A890F0",
+
+  psychic: "hotpink",
+  bug: "#A8B820",
+  dark: "#705848",
+
+  dragon: "#7038F8",
+  steel: "#B8B8D0",
+  fairy: "lightpink",
+};
+export { colors };
