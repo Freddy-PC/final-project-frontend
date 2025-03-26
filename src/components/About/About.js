3eee8c8d1f0e1d2103f3be35bfae43a5a27274a2
Freddy PC <freddythepc@gmail.com>
2025-03-26 17:37:25 -0400

    refatcor: make resuable styling for additional sections in about comp

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,28 @@
 import React from "react";
 import "./About.css";
 import "../App/App.css";
-import AboutPic from "../../images/about-picture.jpg";
+import examplePicture from "../../images/pokemon-background.jpg";
+import Section from "../Section/Section";
 
 function About() {
   return (
     <section className="about app__section">
-      <img src={AboutPic} alt="about-pic" className="about__image" />
+      {/* Section Component here for resuability */}
+      {/* 1. Pass styles as props to Section.js for reuseability
+          2. Pass Class names from About.js 
+          3. Update About.css to have section css
+          4. Format CSS to Figma
+          */}
+      <img src={examplePicture} alt="about-pic" className="about__image" />
       <div className="about__content">
         <h2 className="about__title">About the Author</h2>
         <p className="about__paragraph">
           Hello, my name is Freddy. I’m an experienced Software Engineer who
-          uses HTML, CSS, JavaScript, and React to create webpages.
-        </p>
-        <p className="about__paragraph">
-          Poke-Inspector's mission is to aid Pokemon trainers on their quest to
-          become the greatest Pokemon Scholar of all time! With over 1000+
-          Pokemon, there is so much you need to know “to become the very best
-          like no one ever was”.
-        </p>
-        <p className="about__paragraph">
-          Formerly known as Practicum, TripleTen taught me skills to create both
-          aesthetically pleasing and functional websites. I can't wait to create
-          even more web pages, it feels like embarking on new adventures in
-          unexplored regions of the Pokemon world!
+          uses HTML, CSS, JavaScript, and React to create web-pages!
+          <br></br> I started my Pokémon journey in the Sinnoh region (Gen 4),
+          and since then, I’ve enjoyed learning more about these creatures.
+          Whether they roam the land, sea, or sky, there is something
+          fascinating about each one.
         </p>
       </div>
     </section>
