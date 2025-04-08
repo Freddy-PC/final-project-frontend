285c286d4b184807dc1c1f1554b6662e2c6e0b2d
Freddy PC <freddythepc@gmail.com>
2025-04-08 16:58:00 -0400

    style: arrange elements in a alternating pattern

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,17 +13,33 @@ function About() {
           3. Update About.css to have section css
           4. Format CSS to Figma
           */}
-      <img src={examplePicture} alt="about-pic" className="about__image" />
       <div className="about__content">
-        <h2 className="about__title">About the Author</h2>
-        <p className="about__paragraph">
-          Hello, my name is Freddy. I’m an experienced Software Engineer who
-          uses HTML, CSS, JavaScript, and React to create web-pages!
-          <br></br> I started my Pokémon journey in the Sinnoh region (Gen 4),
-          and since then, I’ve enjoyed learning more about these creatures.
-          Whether they roam the land, sea, or sky, there is something
-          fascinating about each one.
-        </p>
+        <div className="about__row-first">
+          <img src={examplePicture} alt="about-pic" className="about__image" />
+          <div className="about__row-content">
+            <h2 className="about__title">About the Author</h2>
+            <p className="about__paragraph">
+              Hello, my name is Freddy. I’m an experienced Software Engineer who
+              uses HTML, CSS, JavaScript, and React to create web-pages!
+              <br></br> I started my Pokémon journey in the Sinnoh region (Gen
+              4), and since then, I’ve enjoyed learning more about these
+              creatures. Whether they roam the land, sea, or sky, there is
+              something fascinating about each one.
+            </p>
+          </div>
+        </div>
+        <div className="about__row-second">
+          <img src={examplePicture} alt="about-pic" className="about__image" />
+          <div className="about__row-content">
+            <h2 className="about__title">Mission</h2>
+            <p className="about__paragraph">
+              At Poke-Inspector, we’re here to help you on your journey to
+              becoming a true Pokémon expert! With over 1000 Pokémon out there,
+              there’s always something new to discover. So if you’re ready to
+              “be the very best, like no one ever was,” let’s dive in together!
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
