8ab7eef6c28239115810f77e3d0149a8b43f9560
Freddy PC <freddythepc@gmail.com>
2025-04-08 18:02:27 -0400

    refactor: dynamically arrange elemnts and account for multiple positions of child to parent

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,10 +21,12 @@ function About() {
             <p className="about__paragraph">
               Hello, my name is Freddy. I’m an experienced Software Engineer who
               uses HTML, CSS, JavaScript, and React to create web-pages!
-              <br></br> I started my Pokémon journey in the Sinnoh region (Gen
-              4), and since then, I’ve enjoyed learning more about these
-              creatures. Whether they roam the land, sea, or sky, there is
-              something fascinating about each one.
+            </p>
+            <p className="about__paragraph">
+              I started my Pokémon journey in the Sinnoh region (Gen 4), and
+              since then, I’ve enjoyed learning more about these creatures.
+              Whether they roam the land, sea, or sky, there is something
+              fascinating about each one.
             </p>
           </div>
         </div>
@@ -35,8 +37,26 @@ function About() {
             <p className="about__paragraph">
               At Poke-Inspector, we’re here to help you on your journey to
               becoming a true Pokémon expert! With over 1000 Pokémon out there,
-              there’s always something new to discover. So if you’re ready to
-              “be the very best, like no one ever was,” let’s dive in together!
+              there’s always something new to discover.
+            </p>
+            <p className="about__paragraph">
+              So if you’re ready to “be the very best, like no one ever was,”
+              let’s dive in together!
+            </p>
+          </div>
+        </div>
+        <div className="about__row">
+          <img src={examplePicture} alt="about-pic" className="about__image" />
+          <div className="about__row-content">
+            <h2 className="about__title">Acknowledgments</h2>
+            <p className="about__paragraph">
+              Formerly known as Practicum, TripleTen gave me the skills to
+              create websites that are both visually appealing and functional.
+            </p>
+            <p className="about__paragraph">
+              Every new project feels like an adventure into unexplored regions
+              of the Pokémon world, and I can't wait to keep learning and
+              building more!
             </p>
           </div>
         </div>
