652ea1dbc855d25c39f4662152eafba62d416bce
Freddy PC <freddythepc@gmail.com>
2025-04-16 21:29:55 -0400

    style: overall button made but NOT layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,7 @@ import diceIcon from "../../images/dice-icon.svg";
 /* SetQuery is assigned the value of input field
    setIfToggleResult is false at start so result doesn't show in other component 
    lowerCase() method converts uppercase values to lowercase so names are not case-sensitive */
-function Main({ query, searchInput, onSubmit, inputRef, setIfToggleResult }) {
+function Main({ query, searchInput, onSubmit, inputRef, randomSubmit }) {
   return (
     <main className="main app__section">
       <div className="main__container">
@@ -16,37 +16,37 @@ function Main({ query, searchInput, onSubmit, inputRef, setIfToggleResult }) {
           Find information about any Pokemon. Type the Pokemon's name or id
           number according to the national dex!
         </p>
-        <form className="search" onSubmit={onSubmit} noValidate>
-          <input
-            className="search__input"
-            name="search"
-            type="search"
-            id="search"
-            placeholder="Enter name or id number"
-            value={query}
-            onChange={searchInput}
-            ref={inputRef}
-            required
-            minLength="1"
-            maxLength="20"
-          ></input>
+        <div className="search__container">
+          <form className="search" onSubmit={onSubmit} noValidate>
+            <input
+              className="search__input"
+              name="search"
+              type="search"
+              id="search"
+              placeholder="Enter name or id number"
+              value={query}
+              onChange={searchInput}
+              ref={inputRef}
+              required
+              minLength="1"
+              maxLength="20"
+            ></input>
+            <button className="button button__search" type="search">
+              <img className="icon" src={searchIcon} alt="search-icon" />
+              Search
+            </button>
+          </form>
           <button
-            className="search__button"
-            type="search"
-            onClick={() => setIfToggleResult(true)}
+            className="button button__random"
+            type="button"
+            onClick={(e) => {
+              randomSubmit(e);
+            }}
           >
-            <img className="search__icon" src={searchIcon} alt="search-icon" />
-            Search
+            <img className="icon" src={diceIcon} alt="random-icon" />
+            Random
           </button>
-        </form>
-        {/* <button
-          className="random__button"
-          type="random"
-          // onClick={setIfToggleResult}
-        >
-          <img className="random__icon" src={diceIcon} alt="random-icon" />
-          Random
-        </button> */}
+        </div>
       </div>
     </main>
   );
