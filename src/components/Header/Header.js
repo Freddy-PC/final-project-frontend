fa0608a11eca96d5543ea85e3d2adc6b33d0118c
Freddy PC <freddythepc@gmail.com>
2025-04-17 21:24:47 -0400

    style: dynamic text styling and new setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,25 @@ import React from "react";
 import "./Header.css";
 import "../App/App.css";
 import logoImage from "../../images/pokeball-logo.svg";
+import questionLogo from "../../images/question-icon.svg";
 
 function Header() {
   return (
     <header className="header app__section">
       <div className="header__container">
-        <img src={logoImage} alt="Pokeball-Logo" className="header__logo" />
+        <img
+          src={logoImage}
+          alt="Pokeball-Logo"
+          className="header__image header__image_icon"
+        />
         <p className="header__logo-name">Poke-Inspector</p>
+      </div>
+      <div className="header__nav">
+        <img
+          src={questionLogo}
+          alt="Pokeball-Logo"
+          className="header__image header__image_question"
+        />
       </div>
     </header>
   );
