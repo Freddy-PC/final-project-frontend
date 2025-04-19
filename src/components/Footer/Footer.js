31e7608d12948db7cb48911a67618b51d1b50a5c
Freddy PC <freddythepc@gmail.com>
2025-04-19 10:41:31 -0400

    style: add photos for socials and flex

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,28 @@
 import "./Footer.css";
 import "../App/App.css";
+import linkedinLogo from "../../images/linkedin-logo.svg";
+import gitHubLogo from "../../images/github-logo.svg";
 
 function Footer() {
   const today = new Date();
 
   return (
     <footer className="footer app__section">
-      <p className="footer__text">Developed by Freddy Perez-Camacho</p>
-      <p className="footer__text">{today.getFullYear()}</p>
+      <p className="footer__text">
+        @{today.getFullYear()} Freddy Perez-Camacho
+      </p>
+      <div className="footer__images">
+        <img
+          className="image footer__image-lin"
+          alt="linkedIn-logo"
+          src={linkedinLogo}
+        />
+        <img
+          className="image footer__image-git"
+          alt="github-logo"
+          src={gitHubLogo}
+        />
+      </div>
     </footer>
   );
 }
