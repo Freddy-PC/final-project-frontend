ce65f2aaa5f83cb4d4a8c95fa35158070e8db728
Freddy PC <freddythepc@gmail.com>
2025-04-20 15:18:56 -0400

    style: minor padding changes + setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -111,6 +111,7 @@ function App() {
           secondColor={secondColor}
         />
       )}
+      {/* <Trivia/> */}
       <About
         setToggleComponent={setToggleComponent}
         toggleComponent={toggleComponent}
