d31e6d8bd4d1935cee98500679f38e4b4448e6a7
Freddy PC <freddythepc@gmail.com>
2025-04-19 21:00:42 -0400

    refactor/feat: animation after pressing button in about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,69 +13,95 @@ function About({ toggleComponent, setToggleComponent }) {
           3. Update About.css to have section css
           4. Format CSS to Figma
           */}
-      <div className="flex-container">
-        <h1 className="title">About the Author</h1>
-        <p className="paragraph">
-          Like a Pokémon, every developer has a story.{" "}
-          <button
-            className="button button__render button__shadow-drop button__shadow-drop_black"
-            onClick={() => {
-              setToggleComponent(true);
-            }}
+      {toggleComponent == true ? (
+        <div className="about__content">
+          <div
+            className={`about__row ${
+              toggleComponent ? `animation__fade-in` : ""
+            }`}
           >
-            Click here to learn more!✨
-          </button>
-        </p>
-      </div>
-      {toggleComponent == true ? <p>Hey it worked!</p> : <p>This failed!</p>}
-      {/* <div className="about__content">
-        <div className="about__row">
-          <img src={examplePicture} alt="about-pic" className="about__image" />
-          <div className="about__row-content">
-            <h2 className="about__title">About the Author</h2>
-            <p className="about__paragraph">
-              Hello, my name is Freddy. I’m an experienced Software Engineer who
-              uses HTML, CSS, JavaScript, and React to create web-pages!
-            </p>
-            <p className="about__paragraph">
-              I started my Pokémon journey in the Sinnoh region (Gen 4), and
-              since then, I’ve enjoyed learning more about these creatures.
-              Whether they roam the land, sea, or sky, there is something
-              fascinating about each one.
-            </p>
+            <img
+              src={examplePicture}
+              alt="about-pic"
+              className="about__image"
+            />
+            <div className="about__row-content">
+              <h2 className="about__title">About the Author</h2>
+              <p className="about__paragraph">
+                Hello, my name is Freddy. I’m an experienced Software Engineer
+                who uses HTML, CSS, JavaScript, and React to create web-pages!
+              </p>
+              <p className="about__paragraph">
+                I started my Pokémon journey in the Sinnoh region (Gen 4), and
+                since then, I’ve enjoyed learning more about these creatures.
+                Whether they roam the land, sea, or sky, there is something
+                fascinating about each one.
+              </p>
+            </div>
+          </div>
+          <div
+            className={`about__row ${
+              toggleComponent ? `animation__fade-in` : ""
+            }`}
+          >
+            <img
+              src={examplePicture}
+              alt="about-pic"
+              className="about__image"
+            />
+            <div className="about__row-content">
+              <h2 className="about__title">Mission</h2>
+              <p className="about__paragraph">
+                At Poke-Inspector, we’re here to help you on your journey to
+                becoming a true Pokémon expert! With over 1000 Pokémon out
+                there, there’s always something new to discover.
+              </p>
+              <p className="about__paragraph">
+                So if you’re ready to “be the very best, like no one ever was,”
+                let’s dive in together!
+              </p>
+            </div>
+          </div>
+          <div
+            className={`about__row ${
+              toggleComponent ? `animation__fade-in` : ""
+            }`}
+          >
+            <img
+              src={examplePicture}
+              alt="about-pic"
+              className="about__image"
+            />
+            <div className="about__row-content">
+              <h2 className="about__title">Acknowledgments</h2>
+              <p className="about__paragraph">
+                Formerly known as Practicum, TripleTen gave me the skills to
+                create websites that are both visually appealing and functional.
+              </p>
+              <p className="about__paragraph">
+                Every new project feels like an adventure into unexplored
+                regions of the Pokémon world, and I can't wait to keep learning
+                and building more!
+              </p>
+            </div>
           </div>
         </div>
-        <div className="about__row">
-          <img src={examplePicture} alt="about-pic" className="about__image" />
-          <div className="about__row-content">
-            <h2 className="about__title">Mission</h2>
-            <p className="about__paragraph">
-              At Poke-Inspector, we’re here to help you on your journey to
-              becoming a true Pokémon expert! With over 1000 Pokémon out there,
-              there’s always something new to discover.
-            </p>
-            <p className="about__paragraph">
-              So if you’re ready to “be the very best, like no one ever was,”
-              let’s dive in together!
-            </p>
-          </div>
+      ) : (
+        <div className="flex-container">
+          <h1 className="title">About the Author</h1>
+          <p className="paragraph">
+            Like a Pokémon, every developer has a story.{" "}
+            <button
+              className="button button__render button__shadow-drop button__shadow-drop_black"
+              onClick={() => {
+                setToggleComponent(true);
+              }}
+            >
+              Click here to learn more!✨
+            </button>
+          </p>
         </div>
-        <div className="about__row">
-          <img src={examplePicture} alt="about-pic" className="about__image" />
-          <div className="about__row-content">
-            <h2 className="about__title">Acknowledgments</h2>
-            <p className="about__paragraph">
-              Formerly known as Practicum, TripleTen gave me the skills to
-              create websites that are both visually appealing and functional.
-            </p>
-            <p className="about__paragraph">
-              Every new project feels like an adventure into unexplored regions
-              of the Pokémon world, and I can't wait to keep learning and
-              building more!
-            </p>
-          </div>
-        </div>
-      </div> */}
+      )}
     </section>
   );
 }
