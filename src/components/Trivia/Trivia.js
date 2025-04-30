4da6970b4c0bff944198803f48ac3202dd5e6319
Freddy PC <freddythepc@gmail.com>
2025-04-29 20:24:21 -0400

    feat: dynamically render API data into component

--- src/components/Trivia/Trivia.js
+++ src/components/Trivia/Trivia.js
@@ -15,23 +15,37 @@ function Trivia() {
     setPokemonId(id);
 
     getValidSprite(id).then(setPokemonSprite);
-    //   .getPokedexEntry(randomPokemonId)
-    //   .then((pokearray) => {
-    //     setTriviaData(pokearray);
-    //   })
-    //   .catch((err) => console.log(err))
-    //   .finally(() => {});
+    api
+      .getPokedexEntry(id)
+      .then((pokearray) => {
+        setTriviaData(pokearray);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {});
   }, []);
-  // console.log(triviaData);
+
+  // Data won't load on intial render
+  const name = triviaData.names?.["8"].name;
+  const genus = triviaData.genera?.find(
+    (array) => array.language.name === "en"
+  )?.genus;
+  const pokedex = triviaData.flavor_text_entries?.find(
+    (array) => array.language.name === "en"
+  )?.flavor_text;
+
+  console.log(triviaData);
 
   return (
     <section className="trivia app__section">
       <div className="trivia__card">
         <div className="trivia__header">
           <h2 className="trivia__number">{pokemonId}</h2>
-          <h2 className="trivia__name">{}</h2>
+          <h2 className="trivia__name">{name}</h2>
         </div>
-        <div className="trivia__info"></div>
+        <div className="trivia__info">
+          <h2 className="trivia__genus">The {genus}</h2>
+          <h2 className="trivia__pokedex">{pokedex}</h2>
+        </div>
         <div className="trivia__footer"></div>
         <img src={pokemonSprite} />
         {/* alt={`${pokemonData.name} front facing image`} */}
